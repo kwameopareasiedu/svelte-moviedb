f6c4b7793adfb5e12753d28356dfdd7cc0144692
Kwame Opare Asiedu <kwasiedu.5@gmail.com>
2022-06-11 16:51:10 +0000

    Built files for production

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   base: "/svelte-moviedb/",
+  build: { outDir: "docs" },
   plugins: [svelte()],
   server: { port: 8000 }
 });
